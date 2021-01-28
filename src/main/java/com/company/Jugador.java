976481a422d8e3253aa70da969dad4bd41d69c96
ferjozsot <ferjozsot@github.com>
2021-01-27 23:14:20 -0500

    add clases

---
+++ src/main/java/com/company/Jugador.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Jugador {
+    private final String nombreJugador;
+
+    public Jugador(String nombreJugador) {
+        this.nombreJugador = nombreJugador;
+    }
+
+
+    public void mover(Pieza pieza, Casilla posicionInicial, Casilla posicionFinal){
+
+    }
+
+
+}
+
+
