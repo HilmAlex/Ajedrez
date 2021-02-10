a5b73f1b81d260fcae3972f523e93c264ba544cc
ferjozsot <ferjozsot@github.com>
2021-02-10 15:08:20 -0500

    interfaces modificadas

--- src/main/java/com/company/movements/IDiagonalMovement.java
+++ src/main/java/com/company/movements/IDiagonalMovement.java
@@ -1,19 +1,8 @@
 package com.company.movements;
-
 import com.company.Tablero;
 
-import java.util.List;
-
-/**
- * Esta interfaz determina los movimientos diagonales posibles de una pieza de ajedrez
- *      tomando en cuenta las limitaciones en cada pieza.
- * @author Alex Padilla
- */
 public interface IDiagonalMovement {
-    /**
-     * Este método verifica si el movimiento es diagonal comparando la pendiente entre la posicion inicial
-     *      y la posible posicion final, si esta es igual a 1, entonces el movimiento es diagonal.
-     */
+
     default boolean comprobarDiagonal(Tablero tablero, int intColumnaInicio, int filaInicio, int intColumnaFinal, int filaFinal) {
         return Math.abs((intColumnaFinal-intColumnaInicio)/(filaFinal-filaInicio)) == 1 && comprobarInterseccion(tablero, intColumnaInicio, filaInicio, intColumnaFinal, filaFinal);
     }
@@ -27,15 +16,6 @@ public interface IDiagonalMovement {
         return true;
     }
 
-
-    /**
-     * Este metodo calcula los movimientos verticales posibles de una pieza de ajedrez, realiza un
-     *  cálculo de movimientos ascendentes y descendentes a través del método getSquares() de la interfaz
-     *  Imovement para luego unirlos en una sola lista.
-     * @param lockers Casillas del tablero de ajedrez
-     * @param initialPosition Posicion inicial de la pieza
-     * @return Lista de posiciones posibles
-     */
 
 
 
--- src/main/java/com/company/movements/IHorizontalMovement.java
+++ src/main/java/com/company/movements/IHorizontalMovement.java
@@ -1,38 +1,14 @@
 package com.company.movements;
-
-//import com.company.model.Locker;
-//import com.company.model.PositionOnBoard;
-
 import com.company.Tablero;
 
-import java.util.List;
+public interface IHorizontalMovement {
 
-/**
- * Esta interfaz determina los movimientos horizontales posibles de una pieza de ajedrez
- *      tomando en cuenta las limitaciones en cada pieza.
- * @author Alex Padilla
- */
-public interface IHorizontalMovement{
 
-    /**
-     * Este método verifica que el movimiento de una pieza sea horizontal, si la columna inicial
-     *      es igual a la fila final, entonces la pieza realizó un movimiento horizontal
-     * @param //initialPosition Posicion inicial de la pieza
-     * @param //finalPosition Posicion final de la pieza
-     * @return Valor booleano que determina si el movimiento es vertical
-     */
-    default boolean comprobarHorizontal(Tablero tablero,int intColumnaInicio, int filaInicio, int intColumnaFinal, int filaFinal) {
+    default boolean comprobarHorizontal(Tablero tablero, int intColumnaInicio, int filaInicio, int intColumnaFinal, int filaFinal) {
         return intColumnaFinal == intColumnaInicio && comprobarInterseccionHorizontal(tablero, intColumnaInicio, filaInicio, intColumnaFinal, filaFinal);
     }
 
-    /**
-     * Este metodo calcula los movimientos verticales posibles de una pieza de ajedrez, realiza un
-     *  cálculo de movimientos ascendentes y descendentes a través del método getSquares() de la interfaz
-     *  Imovement para luego unirlos en una sola lista.
-     * @param //lockers Casillas del tablero de ajedrez
-     * @param //initialPosition Posicion inicial de la pieza
-     * @return Lista de posiciones posibles
-     */
+
     default boolean comprobarInterseccionHorizontal(Tablero tablero, int intColumnaInicio, int filaInicio, int intColumnaFinal, int filaFinal) {
 
         for (int i = filaInicio + 1; i < filaFinal; i++) {
@@ -40,13 +16,6 @@ public interface IHorizontalMovement{
         }
         return true;
     }
-
-
-
-//    default List<PositionOnBoard> possibleHorizontalMovements(Locker[][] lockers, PositionOnBoard initialPosition) {
-//        List<PositionOnBoard> possiblesMovements;
-//        possiblesMovements = getSquares(lockers, initialPosition, 0,1);
-//        possiblesMovements.addAll(getSquares(lockers,initialPosition, 0,-1));
-//        return possiblesMovements;
-//    }
 }
+
+
